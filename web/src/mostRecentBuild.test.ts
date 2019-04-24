b00524fb9c65f9cd688019622f1796853d6aee10
Dan Miller <dmiller@windmill.engineering>
2019-04-24 17:05:14 -0400

    it's alive

--- web/src/mostRecentBuild.test.ts
+++ web/src/mostRecentBuild.test.ts
@@ -1,4 +1,5 @@
 import mostRecentBuild, { ResourceWithBuilds } from "./mostRecentBuild"
+import { zeroTime } from "./time"
 
 it("returns null if there are no builds", () => {
   const resources: Array<ResourceWithBuilds> = []
@@ -15,6 +16,11 @@ it("returns the most recent build if there are no pending builds", () => {
     FinishTime: "2019-04-24T13:08:42.926608-04:00",
     Log: "",
   }
+  let expectedTuple = {
+    name: "snack",
+    edits: [],
+    since: recent.StartTime,
+  }
   const resource: ResourceWithBuilds = {
     Name: "snack",
     BuildHistory: [
@@ -27,11 +33,37 @@ it("returns the most recent build if there are no pending builds", () => {
       },
       recent,
     ],
-    PendingBuildEdits: ["main.go"],
-    PendingBuildSince: "2019-04-24T13:10:41.317712-04:00",
+    PendingBuildEdits: null,
+    PendingBuildSince: zeroTime,
   }
   const resources: Array<ResourceWithBuilds> = [resource]
 
   let actual = mostRecentBuild(resources)
-  expect(actual).toBe(recent)
+  expect(actual).toEqual(expectedTuple)
+})
+
+it("returns the pending build if there is one", () => {
+  let expectedTuple = {
+    name: "snack",
+    edits: ["bar"],
+    since: "2019-04-24T13:08:41.017623-04:00",
+  }
+  const resource: ResourceWithBuilds = {
+    Name: "snack",
+    BuildHistory: [
+      {
+        Edits: null,
+        Error: null,
+        StartTime: "2019-04-24T13:08:39.017623-04:00",
+        FinishTime: "2019-04-24T13:08:40.926608-04:00",
+        Log: "",
+      },
+    ],
+    PendingBuildEdits: ["bar"],
+    PendingBuildSince: "2019-04-24T13:08:41.017623-04:00",
+  }
+  const resources = [resource]
+
+  let actual = mostRecentBuild(resources)
+  expect(actual).toEqual(expectedTuple)
 })
--- web/src/mostRecentBuild.ts
+++ web/src/mostRecentBuild.ts
@@ -1,4 +1,5 @@
 import { Build } from "./types"
+import { isZeroTime } from "./time"
 
 export type ResourceWithBuilds = {
   Name: string
@@ -7,14 +8,9 @@ export type ResourceWithBuilds = {
   PendingBuildEdits: Array<string> | null
 }
 
-type BuildTuple = {
-  name: string
-  build: Build
-}
-
-const sortByDate = (b1: BuildTuple, b2: BuildTuple) => {
-  let b1Date = Date.parse(b1.b.StartTime)
-  let b2Date = Date.parse(b2.b.StartTime)
+const buildByDate = (b1: BuildTuple, b2: BuildTuple) => {
+  let b1Date = Date.parse(b1.since)
+  let b2Date = Date.parse(b2.since)
   if (b1Date > b2Date) {
     return -1
   }
@@ -24,10 +20,25 @@ const sortByDate = (b1: BuildTuple, b2: BuildTuple) => {
   return 0
 }
 
-const makebuildTuple = (r: ResourceWithBuilds, b: Build): BuildTuple => {
+type BuildTuple = {
+  name: string
+  since: string
+  edits: Array<string>
+}
+
+const makePendingBuild = (r: ResourceWithBuilds): BuildTuple => {
   return {
     name: r.Name,
-    build: b,
+    since: r.PendingBuildSince,
+    edits: r.PendingBuildEdits ? r.PendingBuildEdits : [],
+  }
+}
+
+const makeBuildHistory = (r: ResourceWithBuilds, b: Build): BuildTuple => {
+  return {
+    name: r.Name,
+    since: b.StartTime,
+    edits: b.Edits ? b.Edits : [],
   }
 }
 
@@ -35,13 +46,21 @@ const mostRecentBuild = (
   resources: Array<ResourceWithBuilds>
 ): BuildTuple | null => {
   let r = null
-  // TODO(dmiller): check for pending builds
-  let allBuildsSorted = resources
-    .flatMap(r => r.BuildHistory.map(b => makebuildTuple(r, b)))
-    .sort(sortByDate)
+  let pendingBuildsSorted = resources
+    .map(r => makePendingBuild(r))
+    .filter(b => !isZeroTime(b.since))
+    .sort(buildByDate)
 
-  if (allBuildsSorted.length > 0) {
-    return allBuildsSorted[0]
+  if (pendingBuildsSorted.length > 0) {
+    return pendingBuildsSorted[0]
+  }
+
+  let buildHistorySorted = resources
+    .flatMap(r => r.BuildHistory.map(b => makeBuildHistory(r, b)))
+    .sort(buildByDate)
+
+  if (buildHistorySorted.length > 0) {
+    return buildHistorySorted[0]
   }
 
   return r
